d2dbfb364582d4adc7f839e6113ae339ea68341f
BYPId <a.david.misters@mail.ru>
2024-11-24 16:41:04 +0300

    third part

---
+++ BookingVictoria/BookingVictoria/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BookingSystem
+{
+    public class Booking
+    {
+        public string Username { get; set; }
+        public string ObjectName { get; set; }
+        public DateTime BookingDate { get; set; }
+
+        public Booking(string username, string objectName, DateTime bookingDate)
+        {
+            Username = username;
+            ObjectName = objectName;
+            BookingDate = bookingDate;
+        }
+    }
+}
